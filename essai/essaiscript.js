a5f2be02aef28ed681939ad0e9b57338d715f9f9
Guillaume Coderunner <coderunner2077@gmail.com>
2022-07-16 16:43:00 +0200

    Retrospective Initial commit

---
+++ essai/essaiscript.js
@@ -0,0 +1,12 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="utf-8" />
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<title>Inclusion de HTML5shiv</title>
+	</head>
+	<body>
+	</body>
+</html>
